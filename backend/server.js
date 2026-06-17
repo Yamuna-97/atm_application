d6e6c8c1e2c4f6cd8d4fdd7076aaf1e04f1dca6d
Yamuna <yamunak972006@gmail.com>
2026-06-17 09:02:21 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,9 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: "http://localhost:5173",
+}));
 app.use(express.json());
 
 app.use((err, req, res, next) => {
