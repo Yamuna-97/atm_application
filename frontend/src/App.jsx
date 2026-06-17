37c1bbbcdd3a98b4e54c185e63c66c2dfb70a0e2
Yamuna <yamunak972006@gmail.com>
2026-06-17 09:07:42 +0530

    fix the file name

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
-import Dashboard from "./pages/Dashboard";
+import Login from "./pages/login.jsx";
+import Signup from "./pages/signup.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
 
 function App() {
   const [user, setUser] = useState(null);
