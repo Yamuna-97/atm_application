01930bf959b7270892e17cb17b7d4920d7826fd0
YAMUNA K <yamunak972006@gmail.com>
2026-06-27 19:52:54 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -12,18 +12,15 @@ export const login = (accountNumber, pin) => {
     pin,
   });
 };
-
 export const getBalance = async (accountNumber) => {
   return axios.get(`${API}/balance/${accountNumber}`);
 };
-
 export const deposit = async (accountNumber, amount) => {
   return axios.put(`${API}/deposit`, {
     accountNumber,
     amount,
   });
 };
-
 export const withdraw = async (accountNumber, amount) => {
   return axios.put(`${API}/withdraw`, {
     accountNumber,
